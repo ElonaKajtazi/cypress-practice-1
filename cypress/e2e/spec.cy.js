719fbb193e38bcabbb2e694ca6c985a531ba758d
Elona Kajtazi <MY_NAME@example.com>
2023-02-01 22:06:43 +0100

    Accessing elements and inteacting with them

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
 ///<reference types="cypress" />
 
-it('Should navigate to TodoMVC App', () => {
-  cy.visit('http://todomvc-app-for-testing.surge.sh')
+it('should be able to add a todo', () => {
+  cy.visit('http://todomvc-app-for-testing.surge.sh/')
+  cy.get('.new-todo', {timeout: 6000}).type('Clean room{enter}')
+  cy.get('.toggle').click()
+  cy.contains('Clear').click()
 })
